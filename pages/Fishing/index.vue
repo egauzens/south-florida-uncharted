f4411e9684e6085d9c10bb85db315a1a0f3c797e
Eric Gauzens <ericgauzens@hotmail.com>
2022-10-20 18:38:33 -0400

    update autoplay setting

--- pages/Fishing/index.vue
+++ pages/Fishing/index.vue
@@ -24,7 +24,7 @@
         </div>
       </el-card>
     </VueSlickCarousel>
-    
+
     <div class="m-16">
       <div class="heading1 mb-8">Included: 
         <span class="heading2">Hobie Mirage drive kayak, Rods and reels, PFD, Bait, Tackle, VHF radio, transportation back to launch site, GoPro rental available for additional charge.</span>
@@ -98,7 +98,7 @@ export default {
       settings: {
         dots: true,
         infinite: true,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000,
         speed: 1500,
         slidesToShow: 4,
